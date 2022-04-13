9c91ee3f667f2ea0896bd2fd95e360c83afbb2f2
esutko <esutko@gmail.com>
2022-04-12 23:32:09 -0600

    Update layout to keep cursor further from prompt

--- src/components/Terminal/Prompt.tsx
+++ src/components/Terminal/Prompt.tsx
@@ -1,6 +1,6 @@
 function Prompt(props) {
     return (
-      <span> [user@esutko.com {props.directory}]$ </span>
+      <span className='Prompt'> [user@esutko.com {props.directory}]$ </span>
     );
 }
 
